9edfe9ce2cd9e5fc50dcc001abab2b58cefaa4c2
tezz-cyber <my_name22222222@proton.me>
2026-08-14 12:07:30 +0200

    Images and start script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,15 @@ import node from "@astrojs/node";
 
 export default defineConfig({
   output: "server",
+  site: "https://5.royall-ssd.de",
   adapter: node({ mode: "standalone" }),
+  security: {
+    checkOrigin: true,
+    allowedDomains: [
+      { hostname: "royall-ssd.de" },
+      { hostname: "www.royall-ssd.de" },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
